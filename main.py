ff4c08925e8a2c83f0709e3261f02eeb3b11a1c1
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-05-30 16:33:55 +0430

    add post method
    Using POST method helps to get data from JavaScript and transfer it to Flask.

--- main.py
+++ main.py
@@ -2,13 +2,41 @@ from flask import Flask, render_template
 
 #STATIC_DIR = os.path.abspath('/static')
 
+
 #Create instance
 app = Flask(__name__)
 
 #Route
-@app.route("/")
-def home():
-    return render_template("heart.html")
+@app.route("/", methods = ['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        weight = request.form["weight"]
+        height = request.form["height"]
+        age = request.form["age"]
+        gender = request.form["gender"]
+        ap_h = request.form["ap_h"]
+        ap_l = request.form["ap_l"]
+        chol = request.form["cholestrol"]
+        glu = request.form["glucose"]
+        smoke = request.form["smoke"]
+        al = request.form["alcohol"]
+        excercise = request.form["excercise"]
+        
+        
+        
+        instance = [[int(age), int(gender), int(height), int(weight), int(ap_h), int(ap_l), int(chol), int(glu), int(smoke), int(al), int(excercise) ]]    
+        answer = model.predict([instance])
+        prediction = answer[0]
+        
+        if prediction == 1.0:
+            text = "High risk for the presence of a cardiovascular disease"
+            return redirect(url_for("example", answer = text))
+        if prediction == 0.0:
+            text = "Low risk for the presence of a cardiovascular disease"
+            return redirect(url_for("example", answer = text))
+            
+    else:
+        return render_template("heart.html")
 
 @app.route("/heart.html")
 def heart():
