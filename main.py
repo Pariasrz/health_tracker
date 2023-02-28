d3ccdb2e6e3012ca44b518507df4c5b0a47ed19f
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2023-02-28 13:34:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template
-from MLP import MLP as model
-#STATIC_DIR = os.path.abspath('/static')
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+import pickle
 
 
-#Create instance
-app = Flask(__name__)
+loaded_model = pickle.load(open('/home/pariasrz/health_tracker/pkl_objects/finalized_model.sav', 'rb'))
 
-#information we need to normalize the input of the model
-max_D = [64.9671232876712, 2.00, 200.0, 170.0, 2000.0, 1900.0, 3.0, 3.0, 1.0, 1.0, 1.0] 
+max_D = [64.9671232876712, 2.00, 200.0, 170.0, 2000.0, 1900.0, 3.0, 3.0, 1.0, 1.0, 1.0]
 min_D = [39.1095890410959, 1.0, 120.0, 30.0, 40.0, 40.0, 1.0, 1.0, 0.0, 0.0, 0.0]
 
+app = Flask(__name__)
 
-#Main Route
-@app.route("/", methods = ['POST', 'GET'])
+
+@app.route('/',methods = ['POST', 'GET'])
 def login():
     if request.method == "POST":
         weight = request.form["weight"]
@@ -26,58 +24,70 @@ def login():
         smoke = request.form["smoke"]
         al = request.form["alcohol"]
         excercise = request.form["excercise"]
-        
-        
-        
-        instance = [float(age), float(gender), float(height), float(weight), float(ap_h), float(ap_l), float(chol), float(glu), float(smoke), float(al), float(excercise) ]    
+
+        instance = [float(age), float(gender), float(height), float(weight), float(ap_h), float(ap_l), float(chol), float(glu), float(smoke), float(al), float(excercise) ]
         normalized_instance = []
-        
-        #normalizing the input data of the model
         for i in range(0,11):
             if max_D[i] >= instance[i]:
                 x_max = max_D[i]
             else:
                 x_max = instance[i]
-                
+
             if min_D[i] <= instance[i]:
                 x_min = min_D[i]
             else:
                 x_min = instance[i]
-                
+
             normalized_x = (instance[i] - x_min) / (x_max - x_min)
             normalized_instance.append(normalized_x)
-        
-        #predict the answer using the trained model
-        answer = model.predict([normalized_instance])
+
+        answer = loaded_model.predict([normalized_instance])
+        prob = loaded_model.predict_proba([normalized_instance])[0][1]*100
         prediction = answer[0]
-        prob = (model.predict_proba([normalized_instance])[0][1])*100 #the prbability of the presence of a cardiovascular disease
-        
-        #showing the results
+
         if prediction == 1.0:
             text = "The risk of the presence of a cardiovascular disease is " + str('{:.2f}'.format(prob)) + "% which is high."
-            return redirect(url_for("result", answer = text))
+            return redirect(url_for("example", answer = text))
         if prediction == 0.0:
             text = "The risk of the presence of a cardiovascular disease is " + str('{:.2f}'.format(prob)) + "% which is low."
-            return redirect(url_for("result", answer = text))
-            
+            return redirect(url_for("example", answer = text))
+
     else:
         return render_template("heart.html")
 
-#define other routes
-@app.route("/result.html")
-def result():
+def external_url_handler(error, endpoint, values):
+    "Looks up an external URL when `url_for` cannot build a URL."
+    # This is an example of hooking the build_error_handler.
+    # Here, lookup_url is some utility function you've built
+    # which looks up the endpoint in some external URL registry.
+    url = lookup_url(endpoint, **values)
+    if url is None:
+        # External lookup did not have a URL.
+        # Re-raise the BuildError, in context of original traceback.
+        exc_type, exc_value, tb = sys.exc_info()
+        if exc_value is error:
+            raise (exc_type, exc_value, tb)
+        else:
+            raise error
+    # url_for will use this result, instead of raising BuildError.
+    return url
+
+app.url_build_error_handlers.append(external_url_handler)
+
+@app.route("/example.html")
+def example():
     return render_template("example.html")
 
-@app.route("/heart.html")
-def heart():
-    return render_template("heart.html")
-
+#Define second Route and Content
 @app.route("/bmi.html")
-def about():
+def bmi():
     return render_template("bmi.html")
 
+#Define third Route and Content
 @app.route("/about.html")
 def about():
     return render_template("about.html")
 
-app.run()
+@app.route("/heart.html")
+def heart():
+    return render_template("heart.html")
